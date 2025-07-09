00b9fed2a819e8726fc24ad37be8e0e156c20132
Mahmoud_Abuelhasaan <kingr3319@gmail.com>
2025-07-10 00:54:18 +0300

    init app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_socketio import SocketIO
+from config import Config
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 
 db = SQLAlchemy()
+socketio = SocketIO()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # intialize extinsions
+
+    db.init_app(app)
+    socketio.init_app(app,cors_allowed_origins='*')
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+    bcrypt.init_app(app)
+
+
+    return app
+    
+
+
--- config.py
+++ config.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(base_dir, os.getenv('DATABASE_NAME'))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI =  f'sqlite:///{db_path}'
