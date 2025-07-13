c41086b72c4f2d0a792541e53dc155205231b015
Mahmoud_Abuelhasaan <kingr3319@gmail.com>
2025-07-13 23:16:31 +0300

    fix issue

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from app import  db,create_app,socketio
 app = create_app()
 
 if __name__=='__main__':
-    with app.app_context:
+    with app.app_context():
         db.create_all()
-    socketio.run(app,debug=True,host=0.0.0.0,port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+
 
